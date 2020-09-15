e1fb80cac152db31ee11a7ca6bd916db0c9e154c
jagadeeshkumar25 <63331260+jagadeeshkumar25@users.noreply.github.com>
2020-09-15 19:36:57 +0530

    weatherapp

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 import  requests
 import  json
 from django.views.generic import View
+from datetime import datetime
+
 
 
 # Create your views here.
@@ -22,18 +24,51 @@ class WeatherDetails(View):
 
     def post(self, request):
         city = request.POST.get('c1')
+        date=request.POST.get('b1')
+        bday = datetime.strptime(date, '%Y-%m-%d')
 
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=6f07ddc114fc1213eca9097fb580ba8b'.format(city)
+        x1 = (bday.day)
 
 
-        res = requests.get(url)
+        if x1 > 1:
+            for i in range(2, x1):
+                if (x1 % i) == 0:
+                    message="sorry the given date is not prime plz check another one"
+                    # print("sry the given  date is not prime number")
+                    return render(request, "index.html", {"error_message":message })
 
-        details = res.json()
-        print(details)
-        temp=details['main']['temp_max']
-        pressure=details['main']['pressure']
-        humidity=details['main']['humidity']
-        speed=details['wind']['speed']
-        name=details['name']
-        contex={"temp":temp,"pressure":pressure,"humidity":humidity,"speed":speed,"name":name}
-        return render(request, "index.html", {"data": contex})
+            else:
+                  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=6f07ddc114fc1213eca9097fb580ba8b'.format(
+                    city)
+                  res = requests.get(url)
+                  details = res.json()
+                  temp = details['main']['temp_max']
+                  pressure = details['main']['pressure']
+                  humidity = details['main']['humidity']
+                  speed = details['wind']['speed']
+                  name = details['name']
+                  contex = {"temp": temp, "pressure": pressure, "humidity": humidity, "speed": speed, "name": name}
+                  return render(request, "index.html", {"data": contex})
+        else:
+            message = "sorry the date of  one  is not prime"
+            return render(request, "index.html", {"error_message": message})
+
+        # except
+        #     print(x1, "date must be greater than 1")
+
+        # print(date)
+        #
+        # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=6f07ddc114fc1213eca9097fb580ba8b'.format(city)
+        #
+        #
+        # res = requests.get(url)
+        #
+        # details = res.json()
+        # print(details)
+        # temp=details['main']['temp_max']
+        # pressure=details['main']['pressure']
+        # humidity=details['main']['humidity']
+        # speed=details['wind']['speed']
+        # name=details['name']
+        # contex={"temp":temp,"pressure":pressure,"humidity":humidity,"speed":speed,"name":name}
+        # return render(request, "index.html", {"data": contex})
